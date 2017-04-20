d37732c5f96c81f8a8c5054d211e487af4fd6c55
Yuriy Chernetskiy <lucky.observer@gmail.com>
2017-04-20 08:59:34 +0300

    add numbers to login part of email

--- Home/src/Learning/RandomEmail.java
+++ Home/src/Learning/RandomEmail.java
@@ -1,19 +1,28 @@
 package Learning;
 
 class RandomEmail {
+    private StringBuffer sb;
     private String email;
     private String[] domName = {"@gmail.com", "@hotline.com", "@ya.ru", "@mail.ru", "@i.ua"};
-    private char[] a;
+    private char[] characters;
+    private char[] numbers;
     private int random;
 
     public void newEmail() {
-        a = new char[10];
         random = (int) (0 + Math.random()*5);
-        for (int i = 0; i < a.length; i++) {a[i] += (char) (97+ Math.random() * 25);}
-        email = new String(a);
-        System.out.println(email+domName[random]);
+        // получаем в масcив чаров [a-z]
+        characters = new char[10];
+        for (int i = 0; i < characters.length; i++) {characters[i] += (char) (97+ Math.random() * 25);}
+        email = new String(characters);
+        // получаем в масcив чаров [0-9]
+        numbers = new char[10];
+        for (int i=0; i<10; i++) {numbers[i] = (char) (48 + Math.random() * 10);}
+        // вставляем цифры влогин
+        sb =new StringBuffer(email);
+        for (int i =0; i<4; i++) {sb = sb.insert((int)(1+Math.random()*9), numbers[((int)(1+Math.random()*9))]);}
+        System.out.println(sb+domName[random]);
+        }
     }
-}
 
 class RandomApp {
     public static void main(String[] args) {
